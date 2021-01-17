63b960f2731098d797e50169c32ffb0bd565dff2
nickgs <nick@segosolutions.com>
2021-01-16 19:45:56 -0500

    Updated code to newer version of openzepplin

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -4,5 +4,5 @@ module.exports = function(deployer) {
   const _name = "Long Island Blockchain Token";
   const _symbol = "LIBC";
   const _decimals = 18
-  deployer.deploy(LIBC, _name, _symbol, _decimals);
+  deployer.deploy(LIBC, _name, _symbol);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -46,8 +46,9 @@ module.exports = {
     //
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
+     //  from: "0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b",
     },
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/d06dd91065bc43e6adfcb2ad9f60d6ff`),
@@ -95,15 +96,15 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      version: "0.6.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      docker: false,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
     }
   }
 }
